a4c40ebd6dd4cdd12cbe4b04d718be30b54be9ce
darthpau7 <darthpau7>
2021-03-01 10:36:41 +0100

    solucionado problema 19

--- solutions/2017/prob_19.py
+++ solutions/2017/prob_19.py
@@ -1,10 +1,8 @@
-def escaleras(n):
-    if n > 0:
-        print(" " * (2 * n - 1) + "_" * n)
-        for i in range(n-1):
-            print(" " * (2 * n - i - 2) + "_|")
-        print("_" * n + "|")
-
-
 n = int(input())
-escaleras(n)
+count = n-2
+print(' '*(n+(n-1))+'_'*n)
+
+for x in range(n-1):
+    print(' '*(n + count)+'_|')
+    count -= 1
+print('_'*(n-1)+'|')
