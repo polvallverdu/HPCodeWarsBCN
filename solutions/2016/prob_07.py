c3f17036267320f1bda0a7745bb2feec2eba598c
Arnuito <05.ramal.a@gmail.com>
2021-03-02 22:36:20 +0100

    començat el problema 7 del 2016. cal recordar que el 4 del 2016 no està acabat.ç

---
+++ solutions/2016/prob_07.py
@@ -0,0 +1,14 @@
+time_24h = str(input())
+time = time_24h.split(':')
+
+hours = time[0]
+minutes = time[1]
+seconds = time[2]
+
+row1 = ["-", "-", "-", "-", "-", "-"]
+row2 = ["-", "-", "-", "-", "-", "-"]
+row3 = ["-", "-", "-", "-", "-", "-"]
+row4 = ["-", "-", "-", "-", "-", "-"]
+
+print(row1, "\n", row2, "\n", row3, "\n", row4)
+
