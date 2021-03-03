f0b93839cdce03c4940ed2dad5d37d5f8fc6069f
Arnuito <05.ramal.a@gmail.com>
2021-03-03 16:38:51 +0100

    problema 7 del 2016

--- solutions/2016/prob_07.py
+++ solutions/2016/prob_07.py
@@ -1,14 +1,29 @@
-time_24h = str(input())
-time = time_24h.split(':')
+def decimalToBinary(num):
+    """This function converts decimal number
+    to binary and prints it"""
+    if num > 1:
+        decimalToBinary(num // 2)
+    return num % 2
 
-hours = time[0]
-minutes = time[1]
-seconds = time[2]
+time_24h = str(input()).replace(':', '')
+time = list()
+times_binary = list()
 
-row1 = ["-", "-", "-", "-", "-", "-"]
-row2 = ["-", "-", "-", "-", "-", "-"]
-row3 = ["-", "-", "-", "-", "-", "-"]
-row4 = ["-", "-", "-", "-", "-", "-"]
+for char in time_24h:
+     time.append(int(char))
 
-print(row1, "\n", row2, "\n", row3, "\n", row4)
+for num in time:
+     binary = str(decimalToBinary(num))
+     binaryNums = list()
+     for char in binary:
+          binaryNums.append(char)
+
+for x in range(6):
+     to_print = ''
+     for num in times_binary:
+          if num[x] == 0:
+               to_print += '-'
+          else:
+               to_print += 'o'
+     print(to_print)
 
