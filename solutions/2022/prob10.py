b6c99214763256627cbe6bf354239ed6e3cba15d
Pol Vallverdu <pvallverdualcaide@gmail.com>
2023-02-27 22:13:43 +0100

    tried fixing

--- solutions/2022/prob10.py
+++ solutions/2022/prob10.py
@@ -7,6 +7,9 @@ def check_rank(rank):
     print("Invalid input.")
     exit()
 
+def is_higher_rank(rank1, rank2):
+  return ranks.index(rank1) > ranks.index(rank2)
+
 placeholders = [
   "From: ",
   "From rank: ",
@@ -16,19 +19,26 @@ placeholders = [
   "Timestamp: ",
 ]
 if type == "R":
-  line = input().split(",")
-  print("<<< URGENT >>>")
+  line = input().rstrip().split(",")
+  
+  if is_higher_rank(line[3], line[1]):
+    print("<<< URGENT >>>")
+  
   for x in range(len(placeholders)):
-    if x == 1:
+    if x == 1 or x == 3:
       check_rank(line[x])
     print(placeholders[x] + line[x])
 elif type == "S":
-  inputs = ["URGENT"]
+  inputs = []
   for x in range(6):
-    i = input()
-    if x == 1:
+    i = input().rstrip()
+    if x == 1 or x == 3:
       check_rank(i)
     inputs.append(i)
+  
+  if is_higher_rank(inputs[3], inputs[1]):
+    inputs.insert(0, "URGENT")
+  
   print(",".join(inputs))
 else:
   print("Invalid input.")
